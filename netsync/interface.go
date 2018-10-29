866d028f6ae827bd5ec6af8b12e2cbb97f362bd5
Loki Verloren <l0k1@null.net>
2018-10-29 05:31:24 +0100

    first commit

---
+++ netsync/interface.go
@@ -0,0 +1,41 @@
+// Copyright (c) 2017 The btcsuite developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package netsync
+
+import (
+	"github.com/parallelcointeam/pod/blockchain"
+	"github.com/parallelcointeam/pod/chaincfg"
+	"github.com/parallelcointeam/pod/chaincfg/chainhash"
+	"github.com/parallelcointeam/pod/mempool"
+	"github.com/parallelcointeam/pod/peer"
+	"github.com/parallelcointeam/pod/wire"
+	"github.com/btcsuite/btcutil"
+)
+
+// PeerNotifier exposes methods to notify peers of status changes to
+// transactions, blocks, etc. Currently server (in the main package) implements
+// this interface.
+type PeerNotifier interface {
+	AnnounceNewTransactions(newTxs []*mempool.TxDesc)
+
+	UpdatePeerHeights(latestBlkHash *chainhash.Hash, latestHeight int32, updateSource *peer.Peer)
+
+	RelayInventory(invVect *wire.InvVect, data interface{})
+
+	TransactionConfirmed(tx *btcutil.Tx)
+}
+
+// Config is a configuration struct used to initialize a new SyncManager.
+type Config struct {
+	PeerNotifier PeerNotifier
+	Chain        *blockchain.BlockChain
+	TxMemPool    *mempool.TxPool
+	ChainParams  *chaincfg.Params
+
+	DisableCheckpoints bool
+	MaxPeers           int
+
+	FeeEstimator *mempool.FeeEstimator
+}
