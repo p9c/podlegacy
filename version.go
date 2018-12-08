f2412e3963da3bd991cd3499980bb41c67234439
Loki Verloren <l0k1@null.net>
2018-12-08 01:08:01 +0100

    version bump to v0.1.3-beta

--- version.go
+++ version.go
@@ -1,6 +1,3 @@
-
-
-
 package main
 
 import (
@@ -17,11 +14,11 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 1
-	appPatch uint = 2
+	appPatch uint = 3
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta"
+	appPreRelease = "alpha"
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
